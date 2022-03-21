bbd2ebca3c334c5489d4182c530f41d8270b70d0
Samuel Laforme <laforme.s@hotmail.com>
2022-03-21 19:31:26 -0400

    now find leads based on email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,24 @@ class Customer < ApplicationRecord
   belongs_to :user
   has_one :address
   has_many :building
+  after_save :check_lead
+
+  private
+  def check_lead
+    customer = Customer.last
+    leads = Lead.where(e_mail: customer.email_of_the_company_contact)
+    unless leads.blank?
+      for lead in leads
+        unless lead.attached_file_stored_as_a_binary_file == nil
+          send_to_dropbox
+        end
+      end
+    end
+  end
+
+  def send_to_dropbox
+    p '-----------------------------------------'
+    p 'In send to dropbox'
+    p '-----------------------------------------'
+  end
 end
