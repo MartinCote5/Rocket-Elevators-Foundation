93a6feae8b9f3a2157e4843163fc39233c5738fe
MartinCote5 <martin.cote2@hotmail.com>
2022-03-21 19:36:37 -0400

    loop working

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,3 +1,56 @@
 class Elevator < ApplicationRecord
   belongs_to :column
+
+  def blabla
+
+    p "-----------------------------------------------"
+    p "bbbblblblbbl"
+    p "-----------------------------------------------"
+  end
+
+
+  after_update :elevator_status
+  def elevator_status
+    x = Elevator.all
+    for elevator in x
+      
+     
+      if elevator.status == "Intervention"
+        @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+
+        @client.messages.create(
+        from: ENV["TWILIO_PHONE_NUMBER"],
+        to: '8196769493',
+        body: 'Hey!'
+        )
+        puts "almost there"
+      end
+    end  
+  end
+
+
+
+
+  
+
+
 end
+
+
+
+# def dimcustomers(conn)
+#   customers = Customer.all
+#   for customer in customers
+#     count = 0
+#     buildings = customer.building
+#     city = customer.address.city
+#     for building in buildings
+#       battery = building.battery
+#       columns = battery.column
+#       for column in columns
+#         count += column.elevator.count
+#       end
+#     end
+#     conn.exec("INSERT INTO dimcustomers(creation_date, company_name, full_name_of_the_company_main_contact, email_of_the_company_main_contact, elevator_amount, customer_city)VALUES ('#{customer.created_at}', '#{customer.company_name.gsub(/\'/, '\'\'')}', '#{customer.full_name_of_the_company_contact.gsub(/\'/, '\'\'')}', '#{customer.email_of_the_company_contact}', #{count}, '#{city.gsub(/\'/, '\'\'')}')")
+#   end
+# end
